a38e871e364409703a30d0b62d9f2087a9035fea
oaksas <aman.getnet2@gmail.com>
2022-01-16 11:19:50 -1100

    Update to the UI

--- FakeNews/FakeNews/settings.py
+++ FakeNews/FakeNews/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,18 +38,23 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'News',
+    'corsheaders',
     'rest_framework'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 ROOT_URLCONF = 'FakeNews.urls'
 
--- FakeNews/News/views.py
+++ FakeNews/News/views.py
@@ -10,43 +10,66 @@ key=config('KEY')
 @api_view(('GET',))
 def businessArticles(request,country):  
         requestURL="https://newsapi.org/v2/top-headlines?country={}&category=business&apiKey={}".format(country,key)
-        res=requests.get(requestURL).json()
-        data=res['articles'] 
-        if(not data):
-            return Response(status=404)
-        return Response(data,status=200)
+        try:
+            res=requests.get(requestURL).json()
+            data=res['articles'] 
+            if(not data):
+                return Response(status=404)
+            return Response(data,status=200)
+        except:
+            return Response("No connection", status=502)
+
+       
 
 @api_view(('GET',))
 def allAboutThis(request,subject):
         requestURL="https://newsapi.org/v2/everything?q={}&from=2021-12-10&sortBy=publishedAt&apiKey={}".format(subject,key)
-        res=requests.get(requestURL).json()
-        data=res['articles'] 
-        if(not data):
-            return Response(status=404)
-        return Response(data,status=200)
+        try:
+            res=requests.get(requestURL).json()
+            data=res['articles'] 
+            if(not data):
+                return Response(status=404)
+            return Response(data,status=200)
+
+        except:
+            return Response("No connection", status=502)
+
+       
 
 @api_view(('GET',))
 def topHeadlinesFrom(request,country):
         requestURL="https://newsapi.org/v2/top-headlines?country={}&apiKey={}".format(country,key)
-        res=requests.get(requestURL).json()
-        data=res['articles'] 
-        if(not data):
-            return Response(status=404)
-        return Response(data,status=200)
+        try:
+            res=requests.get(requestURL).json()
+            data=res['articles'] 
+            if(not data):
+                return Response(status=404)
+            return Response(data,status=200)
+        except:
+            return Response("No connection", status=502)
+
+       
 @api_view(('GET',))
 def topHeadlinesFromBBC(request):
         requestURL="https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey={}".format(key)
-        res=requests.get(requestURL).jsonx()
-        data=res['articles'] 
-        if(not data):
-            return Response(status=404)
-        return Response(data,status=200)
+        try:
+            res=requests.get(requestURL).jsonx()
+            data=res['articles'] 
+            if(not data):
+                return Response(status=404)
+            return Response(data,status=200)
+        except:
+            return Response("No connection", status=502)
+
+       
 
 @api_view(('GET','POST'))
 def predictNews(request):
-    news= request.POST.get("news")
+
+    
+
     try:
-       res = Predict(news).predict()
+       res = Predict(request.data['news']).predict()
        return Response(res,status=200)
     except:
         return Response("Internal Error....please Try again ", status=500)
